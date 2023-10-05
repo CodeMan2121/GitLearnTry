9d42bcaee435393b80e207c75114bb8476819b0c
Cebrail <mesecebrail@gmail.com>
2023-10-05 11:27:48 +0300

    hello MASTER

--- GitLearnTry/Program.cs
+++ GitLearnTry/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello MASTER!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dınya!");
