254ade3a961df00beb996a1bf75d442c21f2d73d
Cebrail <mesecebrail@gmail.com>
2023-10-05 11:30:04 +0300

    Hello Featture 1

--- GitLearnTry/Program.cs
+++ GitLearnTry/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(" Feature - 1 ");
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Dınya!");
