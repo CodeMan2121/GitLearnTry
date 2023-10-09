a93179a42a6b2d40e8349fa9dcb79c2cd7eae7dc
Cebrail <mesecebrail@gmail.com>
2023-10-09 16:55:12 +0300

    fixing id to int

--- GitLearnTry/Class1.cs
+++ GitLearnTry/Class1.cs
@@ -12,6 +12,6 @@ namespace GitLearnTry
         public string LastName { get; set; }
         public string Description { get; set; }
         public string Age { get; set; }
-        public id Number {get; set;}
+        public int Number {get; set;}
     }
 }
